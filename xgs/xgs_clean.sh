f1776a5fee0a6587258db91e55f7f8e2b948b069
Baohua Yang <yangbaohua@gmail.com>
2014-05-23 10:44:18 +0800

    Make the script more robust with conditinal validations

--- xgs/xgs_clean.sh
+++ xgs/xgs_clean.sh
@@ -7,18 +7,6 @@
 # Environment information
 #CONTROL_IP=192.168.122.100
 #export OS_AUTH_URL=http://${CONTROL_IP}:35357/v2.0/
-
-source ~/keystonerc_admin
-
-# The tenant, user, net, etc... to be created
-TENANT_NAME="project_one"
-USER_NAME="user"
-USER_PWD="user"
-IMAGE_NAME="ISNP_XGS"
-VM_NAME="xgs"
-ROUTER_NAME="router"
-
-## DO NOT MODIFY THE FOLLOWING PART, UNLESS YOU KNOW WHAT IT MEANS. ##
 echo_r () {
     [ $# -ne 1 ] && return 0
     echo -e "\033[31m$1\033[0m"
@@ -35,6 +23,19 @@ echo_b () {
     [ $# -ne 1 ] && return 0
     echo -e "\033[34m$1\033[0m"
 }
+
+[ ! -e ~/keystonerc_admin ] && echo_r "Not found keystonerc_admin at home directory" && exit -1
+source ~/keystonerc_admin
+
+# The tenant, user, net, etc... to be created
+TENANT_NAME="project_one"
+USER_NAME="user"
+USER_PWD="user"
+IMAGE_NAME="ISNP_XGS"
+VM_NAME="xgs"
+ROUTER_NAME="router"
+
+## DO NOT MODIFY THE FOLLOWING PART, UNLESS YOU KNOW WHAT IT MEANS. ##
 #subnet_name
 get_subnetid_by_name () {
     [ $# -ne 1 ] && return 0
@@ -70,7 +71,9 @@ export OS_TENANT_NAME=${TENANT_NAME}
 export OS_USERNAME=${USER_NAME}
 export OS_PASSWORD=${USER_PWD}
 
-echo_g "Check the xgs vm..."
+echo_g ">>>Starting the XGS clean..."
+
+echo_g "Checking the xgs vm..."
 if [ -n "`nova list|grep ${VM_NAME}`" ]; then
     VM_ID=`nova list|grep ${VM_NAME}|awk '{print $2}'`
     echo_g "Deleting the xgs vm..."
@@ -80,18 +83,18 @@ fi
 
 source ~/keystonerc_admin
 
-echo_g "Check the router interfaces..."
+echo_g "Checking the router interfaces..."
 ROUTER_ID=`neutron router-list|grep ${ROUTER_NAME}|awk '{print $2}'`
 INF_ID=$(get_subnetid_by_name private-subnet1)
 if [ -n "${ROUTER_ID}" -a -n "${INF_ID}" ]; then 
-    echo"Deleting its interface from the private1 subnet..."
+    echo "Deleting its interface from the private1 subnet..."
     neutron router-interface-delete ${ROUTER_ID} ${INF_ID}
 fi
 
-echo_g "Clear the xgs nets and subnets..."
+echo_g "Clearing the xgs nets and subnets..."
 delete_net_subnet "private1" "private-subnet1"
 delete_net_subnet "private2" "private-subnet2"
 delete_net_subnet "private3" "private-subnet3"
 delete_net_subnet "private4" "private-subnet4"
 
-echo_g "Done"
+echo_g "<<<Done" && exit 0
--- xgs/xgs_init.sh
+++ xgs/xgs_init.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 #Follow the steps at https://w3-connections.ibm.com/wikis/home?lang=en-us#!/wiki/Stefan's%20Corner/page/Havana%20--%20XGSPlugin%20Installation%20Instructions%20V1
+#https://w3-connections.ibm.com/wikis/home?lang=en-us#!/wiki/Stefan's%20Corner/page/IceHouse%20--%20XGSPlugin%20Installation%20Instructions
+
+echo_r () {
+    [ $# -ne 1 ] && return 0
+    echo -e "\033[31m$1\033[0m"
+}
+echo_g () {
+    [ $# -ne 1 ] && return 0
+    echo -e "\033[32m$1\033[0m"
+}
+echo_y () {
+    [ $# -ne 1 ] && return 0
+    echo -e "\033[33m$1\033[0m"
+}
+echo_b () {
+    [ $# -ne 1 ] && return 0
+    echo -e "\033[34m$1\033[0m"
+}
+
 
 ## THOSE VARIABLES CAN BE CUSTOMIZED. ##
 
@@ -9,6 +28,7 @@
 #export OS_TENANT_NAME=admin
 #export OS_USERNAME=admin
 #export OS_PASSWORD=admin
+[ ! -e ~/keystonerc_admin ] && echo_r "Not found keystonerc_admin at home directory" && exit -1
 source ~/keystonerc_admin
 
 ADMIN_NAME=admin #the name of the default admin user
@@ -24,7 +44,7 @@ USER_ROLE="_member_"
 USER_ROLE2="Member"
 
 IMAGE_NAME="ISNP_XGS"
-IMAGE_FILE=ISNP_5.1.2_20131121-1829.qcow2
+IMAGE_FILE=ISNP_5.2_20140502-1219_personal_compat.qcow2
 IMAGE_INITED_NAME="ISNP_XGS_INITED"
 IMAGE_INITED_FILE=xgs_inited.qcow2
 VM_NAME="xgs"
@@ -49,7 +69,6 @@ get_subnetid_by_name () {
     echo `neutron subnet-list|grep ${NAME}|awk '{print $2}'`
 }
 
-
 #net_name, subnet_name, ip_cidr, gateway,
 create_net_subnet () {
     [ $# -ne 4 ] && echo "Wrong parameter number is given" && exit -1
@@ -61,14 +80,18 @@ create_net_subnet () {
     [ -z "`neutron subnet-list|grep ${SUBNET_NAME}`" ] && neutron subnet-create --tenant-id ${TENANT_ID} --name ${SUBNET_NAME} ${NET_NAME} ${IP_CIDR} --gateway ${GATEWAY} --dns_nameservers list=true 8.8.8.7 8.8.8.8
 }
 
-echo "Check the vm image..."
-[ -f ${IMAGE_FILE} ] || exit 0;
 
-echo "Check tenant ${TENANT_NAME}"
+echo_g ">>>Starting the XGS initialization..."
+
+echo_g "Checking the vm image..."
+[ ! -f ${IMAGE_FILE} ] && echo_r "vm image ${IMAGE_FILE} not found" exit -1;
+
+echo_g "Checking tenant ${TENANT_NAME}"
 [ -z "`keystone tenant-list|grep ${TENANT_NAME}`" ] && keystone tenant-create --name ${TENANT_NAME} --description "${TENANT_DESC}"
 TENANT_ID=`keystone tenant-list|grep ${TENANT_NAME}|awk '{print $2}'`
+echo "tenant id = ${TENANT_ID}"
 
-echo "Check user and try to add it into the tenant..."
+echo_g "Checking user and try to add it into the tenant..."
 [ -z "`keystone user-list|grep ${USER_NAME}`" ] && keystone user-create --name ${USER_NAME} --pass ${USER_PWD} --tenant-id ${TENANT_ID} --email ${USER_EMAIL}
 USER_ID=`keystone user-list|grep ${USER_NAME}|awk '{print $2}'`
 if [ -n "`keystone role-list|grep ${USER_ROLE}`" ]; then
@@ -76,34 +99,43 @@ if [ -n "`keystone role-list|grep ${USER_ROLE}`" ]; then
 elif [ -n "`keystone role-list|grep ${USER_ROLE2}`" ]; then
     ROLE_ID=`keystone role-list|grep ${USER_ROLE2}|awk '{print $2}'`
 else
-    echo "No role is found"
+    echo_r "No role is found"
     exit -1;
 fi
 [ -z "`keystone user-role-list --tenant-id ${TENANT_ID} --user-id ${USER_ID}|grep ${ROLE_ID}`" ] && keystone user-role-add --tenant-id ${TENANT_ID} --user-id ${USER_ID} --role-id ${ROLE_ID}
 
-echo "Create nets and subnets"
+echo_g "Creating 4 nets and subnets"
 create_net_subnet "private1" "private-subnet1" "10.0.1.0/24" "10.0.1.1"
 create_net_subnet "private2" "private-subnet2" "10.0.2.0/24" "10.0.2.1"
 create_net_subnet "private3" "private-subnet3" "10.0.3.0/24" "10.0.3.1"
 create_net_subnet "private4" "private-subnet4" "10.0.4.0/24" "10.0.4.1"
 
-echo "Check the router, add its interface to the private1 subnet..."
+echo_g "Checking the router, add its interface to the private1 subnet..."
 [ -z "`neutron router-list|grep ${ROUTER_NAME}`" ] && neutron router-create --tenant-id ${TENANT_ID} ${ROUTER_NAME}
 ROUTER_ID=`neutron router-list|grep ${ROUTER_NAME}|awk '{print $2}'`
-#neutron router-interface-add ${ROUTER_ID} $(get_subnetid_by_name private-subnet1)
-
-echo "Add the image file into glance and create flavors..."
-if [ -z "`glance image-list|grep ${IMAGE_NAME}`" ]; then
-    glance image-create --disk-format qcow2 --container-format bare --name ${IMAGE_NAME} --is-public True --file ${IMAGE_FILE} --progress
-    glance image-update --property hw_disk_bus=ide --property hw_vif_model=e1000 ${IMAGE_NAME}
-    sleep 2
+INF_ID=$(get_subnetid_by_name private-subnet1)
+if [ -n "${ROUTER_ID}" -a -n "${INF_ID}" ]; then 
+    echo "Adding its interface into the private1 subnet..."
+    neutron router-interface-add ${ROUTER_ID} ${INF_ID}
 fi
-if [ -z "`glance image-list|grep ${IMAGE_INITED_NAME}`" ]; then
+
+echo_g "Adding the image file into glance..."
+if [ -f ${IMAGE_FILE} -a -z "`glance image-list|grep ${IMAGE_NAME}`" ]; then
+    echo "Creating glance image ${IMAGE_NAME}"
+    glance image-create --disk-format qcow2 --container-format bare --name ${IMAGE_NAME} --is-public True --file ${IMAGE_FILE} --progress
+    glance image-update --property hw_disk_bus=ide --property hw_vif_model=rtl8139 ${IMAGE_NAME}
+    sleep 1
+fi
+if [ -f ${IMAGE_FILE} -a -z "`glance image-list|grep ${IMAGE_INITED_NAME}`" ]; then
+    echo "Creating glance image ${IMAGE_INITED_NAME}"
     glance image-create --disk-format qcow2 --container-format bare --name ${IMAGE_INITED_NAME} --is-public True --file ${IMAGE_INITED_FILE} --progress
-    glance image-update --property hw_disk_bus=ide --property hw_vif_model=e1000 ${IMAGE_INITED_NAME}
-    sleep 2
+    glance image-update --property hw_disk_bus=ide --property hw_vif_model=rtl8139 ${IMAGE_INITED_NAME}
+    sleep 1
 fi
 IMAGE_ID=`glance image-list|grep ${IMAGE_NAME}|awk '{print $2}'`
+[ -z "${IMAGE_ID}" ] && echo_r "No image is created successfully" && exit -1
+
+echo_g "Creating new flavor..."
 [ -z "`nova flavor-list|grep tmp.xgs`" ] && nova flavor-create --is-public true tmp.xgs 20 1024 10 1
 
 #change to user and add security rules, then start a vm
@@ -111,7 +143,7 @@ export OS_TENANT_NAME=${TENANT_NAME}
 export OS_USERNAME=${USER_NAME}
 export OS_PASSWORD=${USER_PWD}
 
-echo "Boot a vm in the internal net..."
+echo_g "Booting a vm in the internal net..."
 nova boot ${VM_NAME} --image ${IMAGE_ID} --flavor 20 --availability-zone az1 \
 --nic net-id=$(get_netid_by_name private1) \
 --nic net-id=$(get_netid_by_name private2) \
@@ -120,5 +152,4 @@ nova boot ${VM_NAME} --image ${IMAGE_ID} --flavor 20 --availability-zone az1 \
 
 sleep 2;
 
-echo "Done"
-exit
+echo_g "<<<Done" && exit 0
